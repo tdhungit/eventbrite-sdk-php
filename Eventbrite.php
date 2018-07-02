27a2eb59a39da09523bdb00b88c35925e156d4bb
Jacky <tdhungit@gmail.com>
2018-07-02 15:45:51 +0700

    update api

--- Eventbrite.php
+++ Eventbrite.php
@@ -75,10 +75,62 @@ class Eventbrite
     }
 
     /**
+     * get profile current user
      * @return array|mixed
      */
     public function getProfile()
     {
         return $this->get('/users/me/');
+    }
+
+    /**
+     * create a event
+     * https://www.eventbrite.com/developer/v3/endpoints/events/#ebapi-post-events
+     * @param $event
+     * @Example $event = [
+        "event.name.html" => "Test Event 01",
+        "event.description.html" => "Test Event 01",
+        "event.start.timezone" => "America/Chicago",
+        "event.start.utc" => "2017-07-10T18:00:00Z", // format Y-m-d\TH:i:s\Z
+        "event.end.timezone" => "America/Chicago",
+        "event.end.utc" => "2017-07-10T20:00:00Z", // format Y-m-d\TH:i:s\Z
+        "event.currency" => "USD",
+       ];
+     * @return array|mixed
+     */
+    public function createEvent($event)
+    {
+        return $this->post('/events/', $event);
+    }
+
+    /**
+     * get detail a event
+     * @param $eventId
+     * @return array|mixed
+     */
+    public function getEvent($eventId)
+    {
+        return $this->get('/events/' . $eventId . '/');
+    }
+
+    /**
+     * Publishes an event
+     * @param $eventId
+     * @return array|mixed
+     */
+    public function publishEvent($eventId)
+    {
+        return $this->post('/events/' . $eventId . '/publish/');
+    }
+
+    /**
+     * get all attendees of event
+     * @param $eventId
+     * @return array|mixed
+     */
+    public function getAttendees($eventId)
+    {
+        echo $path = '/events/' . $eventId . '/attendees/';
+        return $this->get($path);
     }
 }
--- example/index.php
+++ example/index.php
@@ -12,9 +12,30 @@
 require_once __DIR__ . '/../Eventbrite.php';
 
 $config = require_once __DIR__ . '/config.php';
-
 $eventObject = new Eventbrite($config);
-$user = $eventObject->getProfile();
 
 echo '<pre>';
-print_r($user);
+
+//echo 'Current User:<br>';
+//$user = $eventObject->getProfile();
+//print_r($user);
+
+//echo '<br><br>Create Event:<br>';
+//$event = [
+//    "event.name.html" => "Test Event 01",
+//    "event.description.html" => "Test Event 01",
+//    "event.start.timezone" => "America/Los_Angeles",
+//    "event.start.utc" => date('Y-m-d\TH:i:s\Z', strtotime("+2 week 2 days")),
+//    "event.end.timezone" => "America/Los_Angeles",
+//    "event.end.utc" => date('Y-m-d\TH:i:s\Z', strtotime("+20 week 2 days")),
+//    "event.currency" => "USD",
+//];
+//print_r($event);
+//$createEvent = $eventObject->createEvent($event);
+//echo '<br>Result:<br>';
+//print_r($createEvent);
+
+echo '<br><br>Get Attendees:<br>';
+$attendees = $eventObject->getAttendees('47658505874');
+echo '<br>Result:<br>';
+print_r($attendees);
